9ade2dd2c6459d16dcc3d4757f947a5d91e48938
Carlos Bahia <carlosj.sbahia@gmail.com>
2025-09-16 22:12:13 -0400

    Aula 02 - Manipulando Variáveis.

---
+++ modulo07-javascript-basico/aula02-manipulando-variaveis/script.js
@@ -0,0 +1,27 @@
+var curso = "JavaScript";
+alert(curso);
+
+curso = "PHP";
+alert(curso);
+
+curso = 150;
+alert(curso);
+
+curso = "Curso JavaScript";
+alert(curso);
+
+alert(curso + ' Sujeito Programador!');
+
+var valor1 = 7;
+alert(valor1);
+
+var valor2 = 5;
+alert(valor1 + valor2);
+
+var soma = valor1 + valor2;
+alert(soma);
+
+soma = (valor1 + valor2) / 2;
+alert(soma);
+
+alert("Media do aluno X: " + soma);
