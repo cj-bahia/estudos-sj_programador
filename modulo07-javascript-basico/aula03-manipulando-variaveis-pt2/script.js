c0c65ca654659146d5d2a355a92ac612753fcc6c
Carlos Bahia <carlosj.sbahia@gmail.com>
2025-09-16 22:32:03 -0400

    Aula 03 - Manipulando variáveis parte 2.

---
+++ modulo07-javascript-basico/aula03-manipulando-variaveis-pt2/script.js
@@ -0,0 +1,17 @@
+var nome = "Matheus";
+
+alert(nome);
+
+// alert('N'ome); não exibirá "Matheus" pois existe case sensitive
+
+var Nome = "Carlos";
+
+alert(Nome); // agora exibe 'Nome'
+
+// não é possível usar keys words para nome de variáveis
+// key word: var, functions, etc
+
+var val1 = 10;
+var val2 = 2;
+
+console.log(val1+val2);
