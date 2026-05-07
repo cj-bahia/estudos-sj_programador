8453de7652e76fdccc947f1ee447333753b8fca1
cj-bahia <carlosj.sbahia@gmail.com>
2026-05-07 17:10:55 -0400

    Aula 04 - Comandos de entrada

---
+++ modulo07-javascript-basico/aula04-comandos-entrada/script.js
@@ -0,0 +1,11 @@
+var nome = prompt("Qual seu nome?"); //prompt é um comando de entrada, ele abre uma caixa de diálogo para o usuário digitar algo, e o valor digitado é armazenado na variável nome
+
+var sobrenome = prompt("Qual seu sobrenome?");
+
+console.log("Olá " + nome + " " + sobrenome);
+
+document.write("<h1>Bem vindo " + nome + " " + sobrenome + " ao projeto</h1>"); //Escreve na tela do navegador
+
+//Script lido de cima para baixo!
+
+document.write("<img src= 'https://sujeitoprogramador.com/steve.png' alt='Foto do Steve' />"); //Põe imagem na tela do navegador
