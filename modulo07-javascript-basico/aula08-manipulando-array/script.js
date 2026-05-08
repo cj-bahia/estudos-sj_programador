1d88c8649a7c8dd62b22c5fdd8c55bbc41574c07
cj-bahia <carlosj.sbahia@gmail.com>
2026-05-08 13:39:17 -0400

    Aula 08 - manipulando arrays

---
+++ modulo07-javascript-basico/aula08-manipulando-array/script.js
@@ -0,0 +1,13 @@
+var lista = ['Lucas', 'Jose', 'Maria', 150];
+
+// lista.indexOf() Retorna o índice do elemento especificado na lista, ou -1 se não for encontrado
+
+// lista[0] = 'Carlos'; Altera o valor do elemento no índice 0 da lista para 'Carlos'
+
+// lista.push('Jaasiel'); Adiciona um novo elemento 'Jaasiel' ao final da lista
+
+// lista.pop(); Remove o último elemento da lista
+
+// lista.shift(); Remove o primeiro elemento da lista
+
+// lista.join(', '); Retorna uma string concatenada com os elementos da lista, separados por ', '
