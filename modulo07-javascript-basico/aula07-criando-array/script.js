c2da5adb8cd6b0a6b417a3faca85c64040c0c012
cj-bahia <carlosj.sbahia@gmail.com>
2026-05-08 12:21:10 -0400

    Aula 07 - criando arrays

---
+++ modulo07-javascript-basico/aula07-criando-array/script.js
@@ -0,0 +1,5 @@
+var lista = ['Carlos', 'Jaasiel', 'Souza', 'Bahia', 18]; // Criando um array chamado "lista" com elementos de diferentes tipos (string e número)
+
+// console.log(lista[1]); Acessando o elemento do índice 1 da lista
+
+// lista.length; Retorna o número de elementos presentes na lista
