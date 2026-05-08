66361675446e44839dcd239632cabcd0993af7ac
cj-bahia <carlosj.sbahia@gmail.com>
2026-05-08 13:50:50 -0400

    Aula 09 - Conhecendo loops

---
+++ modulo07-javascript-basico/aula09-loops/script.js
@@ -0,0 +1,23 @@
+// while = enquanto
+/*
+var x = 5;
+
+while (x < 10){  // enquanto x for menor que 10, o código dentro do bloco será executado
+    document.write('O valor do x é: ' + x + '<br>');
+    
+    x++; // Incrementa o valor de x em 1 a cada iteração do loop
+}
+*/
+
+// for = para
+/*
+for (a=0; a <= 10; a++){ // para a variável a, enquanto a for menor e igual a 10, incrementa a em 1 a cada iteração do loop
+    document.write('Valor do A é: ' + a + '<br>');
+}
+*/
+
+var valor = 15;
+
+for (a=0; a <= valor; a++){ // para a variável a, enquanto a for menor e igual ao valor da variável "valor", incrementa a em 1 a cada iteração do loop
+    document.write('Valor do A é: ' + a + '<br>');
+}
