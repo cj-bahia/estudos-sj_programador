310f0860253d1af86adcb14fa73bbabcd51656aa
cj-bahia <carlosj.sbahia@gmail.com>
2026-05-11 19:59:55 -0400

    Aula 11 - Condicionais e Comparação

---
+++ modulo07-javascript-basico/aula11-condicionais-e-comparacao/script.js
@@ -0,0 +1,7 @@
+var valor = "30"; // string
+
+if (valor === 30){ // === comparação de valor e tipo | == comparação de valor
+    console.log("Sim o valor é igual a 30!");
+}else{
+    console.log("Valor diferente de 30!");
+}
