8259a1285b6e2490ec5405eb83a2712577b81c86
cj-bahia <carlosj.sbahia@gmail.com>
2026-05-11 21:52:12 -0400

    Aula 06 - Template Strings

---
+++ modulo08-javascript-avancado/aula06-template-string/script.js
@@ -0,0 +1,12 @@
+let nome = "Carlos";
+let sobrenome = "Bahia";
+let idade = 19;
+
+let mensagem = "Meu nome é " + nome + " " + sobrenome + " e tenho " + idade + " anos.";
+
+console.log(mensagem);
+
+// Template String
+let mensagemTemplate = `Meu nome é ${nome} ${sobrenome} e tenho ${idade} anos.`;
+
+console.log(mensagemTemplate);
