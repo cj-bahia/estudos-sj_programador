225664ef54bb7b7d8807b1642fb77f154105cafe
cj-bahia <carlosj.sbahia@gmail.com>
2026-05-11 20:07:06 -0400

    Aula 12 - Condicionais e Comparação, operador ternário

---
+++ modulo07-javascript-basico/aula12-condicionais-e-comparacao-2/script.js
@@ -0,0 +1,25 @@
+var valor = "Carlos"; // string
+
+if (valor == "Carlos"){ // == comparação de valor
+    console.log("Sim o nome é igual a Carlos!");
+}else{
+    console.log("Nome diferente de Carlos!");
+}
+
+// ---------------------------------------------------
+
+var userOnline = true; // boolean
+
+if (userOnline === true){ // === comparação de valor e tipo
+    console.log("Usuário online!");
+}else{
+    console.log("Usuário offline!");
+}
+
+// ---------------------------------------------------
+
+// Operador ternário
+
+var numero = 10;
+
+numero === 10 ? console.log("Número é igual a 10!") : console.log("Número diferente de 10!");
