7733b3c8a5c1c60e522d9bcf47cea0a7dcffadf8
cj-bahia <carlosj.sbahia@gmail.com>
2026-05-11 23:46:36 -0400

    Aula 03 - Listando as tarefas no index

--- modulo09-projeto04-javascript/script.js
+++ modulo09-projeto04-javascript/script.js
@@ -5,6 +5,20 @@ let buttonElement = document.querySelector("#app button");
 
 let tarefas = []; // Array para armazenar as tarefas
 
+function renderizarTarefas(){
+
+    listElement.innerHTML = ''; // Limpando a lista antes de renderizar
+
+    tarefas.map((toDo) => { // Iterando sobre o array de tarefas usando o método map
+        let liElement = document.createElement("li"); // Criando um elemento <li> para cada tarefa
+        let tarefaText = document.createTextNode(toDo); // Criando um nó de texto com o conteúdo da tarefa
+
+        liElement.appendChild(tarefaText); // Adicionando o texto da tarefa ao elemento <li>
+        listElement.appendChild(liElement); // Adicionando o elemento <li> à lista <ul>
+    });
+
+}
+
 function adicionarTarefas(){
     
     if (inputElement.value === ''){
@@ -15,6 +29,8 @@ function adicionarTarefas(){
 
         tarefas.push(novaTarefa); // Adicionando a nova tarefa ao array
         inputElement.value = ''; // Limpando o campo de input
+
+        renderizarTarefas(); // Chamando a função para renderizar as tarefas atualizadas
     }
 
 }
