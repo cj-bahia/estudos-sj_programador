eac9fd42ccbd6d91b0b01f0da744ba01a458c9f7
cj-bahia <carlosj.sbahia@gmail.com>
2026-05-12 00:02:03 -0400

    Aula 04 - Deletando tarefas

--- modulo09-projeto04-javascript/script.js
+++ modulo09-projeto04-javascript/script.js
@@ -13,7 +13,18 @@ function renderizarTarefas(){
         let liElement = document.createElement("li"); // Criando um elemento <li> para cada tarefa
         let tarefaText = document.createTextNode(toDo); // Criando um nó de texto com o conteúdo da tarefa
 
+        let linkElement = document.createElement("a"); // Criando um elemento <a> para o link de exclusão
+        linkElement.setAttribute("href", "#"); // Definindo o atributo href do link para "#" (pode ser ajustado conforme necessário)
+
+        let linkText = document.createTextNode("Excluir"); // Criando um nó de texto para o link de exclusão
+        linkElement.appendChild(linkText); // Adicionando o texto ao elemento <a>
+
+        let posicao = tarefas.indexOf(toDo);
+
+        linkElement.setAttribute("onclick", `deletarTarefas(${posicao})`); // Definindo o atributo onclick do link para chamar a função de exclusão (pode ser ajustado conforme necessário)
+
         liElement.appendChild(tarefaText); // Adicionando o texto da tarefa ao elemento <li>
+        liElement.appendChild(linkElement); // Adicionando o link de exclusão ao elemento <li>
         listElement.appendChild(liElement); // Adicionando o elemento <li> à lista <ul>
     });
 
@@ -34,5 +45,10 @@ function adicionarTarefas(){
     }
 
 }
+function deletarTarefas(posicao){
+    tarefas.splice(posicao, 1); // Removendo a tarefa do array com base na posição
+    renderizarTarefas(); // Chamando a função para renderizar as tarefas atualizadas
+}
+
 
 buttonElement.onclick = adicionarTarefas; // Atribuindo a função ao evento de clique do botão
