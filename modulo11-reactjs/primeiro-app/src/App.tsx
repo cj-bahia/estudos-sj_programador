f81643a6284261b53f42aefbda297345cae6d2e4
cj-bahia <carlosj.sbahia@gmail.com>
2026-05-18 02:37:44 -0400

    Aula 06 - Praticando com simples to-do list

--- modulo11-reactjs/primeiro-app/src/App.tsx
+++ modulo11-reactjs/primeiro-app/src/App.tsx
@@ -1,27 +1,15 @@
 import { useState } from "react";
 
-interface User { // Interface para tipagem
-  nome?: string; // '?' significa que o campo pode ser nulo, sem ele os campos seriam obrigatórios
-  idade?: number;
-  email?: string;
-}
-
 function App(){
 
-  const [nome, setNome] = useState('');
-  const [idade, setIdade] = useState(0);
-  const [email, setEmail] = useState('');
-
-  const [user, setUser] = useState<User>({}); // <User> significa que o estado user irá receber um objeto do tipo User (Interface criada acima)
+  const [input, setInput] = useState('');
+  const [tarefas, setTarefas] = useState<string[]>([]); // Precisa do <string[]> para indicar que o array irá conter strings
 
   function handleRegister(event: React.SyntheticEvent<HTMLFormElement>){ // Função para lidar com o envio do formulário
     event.preventDefault();
 
-    setUser({ // Atualizando o estado do componente
-      nome: nome,
-      idade: idade,
-      email: email,
-    });
+    setTarefas([...tarefas, input]); // Adiciona a tarefa na lista
+    setInput(''); // Limpa o input
   }
 
   return(
@@ -29,31 +17,12 @@ function App(){
       <form onSubmit={ handleRegister }>
         <h1>Cadastrando usuário</h1>
 
-        <label>Nome:</label>
+        <label>Nome da tarefa:</label>
         <br />
         <input
-          placeholder="Digite seu nome" 
-          value={nome}
-          onChange={ ( event ) => setNome(event.target.value)}    
-        />
-        <br />
-
-        <label>Idade:</label>
-        <br />
-        <input
-          type="number"
-          placeholder="Digite sua idade"
-          value={idade}
-          onChange={ ( event ) => setIdade(Number(event.target.value))}  
-        />
-        <br />
-
-        <label>Email:</label>
-        <br />
-        <input
-          placeholder="Digite sua email"
-          value={email}
-          onChange={ ( event ) => setEmail(event.target.value)}    
+          placeholder="Digite uma tarefa" 
+          value={input}
+          onChange={ ( event ) => setInput(event.target.value)}    
         />
         <br />
 
@@ -61,18 +30,13 @@ function App(){
 
         <br /><br />
 
-        <div>
+        <ul>
 
-          <span>Bem vindo: {user.nome}</span>
-          <br />
-          
-          <span>Idade: {user.idade}</span>
-          <br />
+          {tarefas.map(tarefa => ( // Mapeia as tarefas na lista
+            <li key={tarefa}>{tarefa}</li> // Exibe cada tarefa, 'key' necessário para identificar cada tarefa
+          ))}
 
-          <span>Email: {user.email}</span>
-          <br />
-
-        </div>
+        </ul>
 
       </form>
 
