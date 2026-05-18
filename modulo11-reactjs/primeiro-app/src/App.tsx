5fcc40f87be5671d688566a2b160ba874ffc9a35
cj-bahia <carlosj.sbahia@gmail.com>
2026-05-18 02:22:45 -0400

    Aula 05 - Manipulando formulários

--- modulo11-reactjs/primeiro-app/src/App.tsx
+++ modulo11-reactjs/primeiro-app/src/App.tsx
@@ -1,24 +1,83 @@
 import { useState } from "react";
 
-import Nome from "./components/Nome";
+interface User { // Interface para tipagem
+  nome?: string; // '?' significa que o campo pode ser nulo, sem ele os campos seriam obrigatórios
+  idade?: number;
+  email?: string;
+}
 
 function App(){
-    const[aluno, setAluno] = useState('Sujeito Programador');
 
-    function handleChangeName(nome: string){
-        setAluno(nome);
-    }
+  const [nome, setNome] = useState('');
+  const [idade, setIdade] = useState(0);
+  const [email, setEmail] = useState('');
 
-    return(
+  const [user, setUser] = useState<User>({}); // <User> significa que o estado user irá receber um objeto do tipo User (Interface criada acima)
+
+  function handleRegister(event: React.SyntheticEvent<HTMLFormElement>){ // Função para lidar com o envio do formulário
+    event.preventDefault();
+
+    setUser({ // Atualizando o estado do componente
+      nome: nome,
+      idade: idade,
+      email: email,
+    });
+  }
+
+  return(
+    <div>
+      <form onSubmit={ handleRegister }>
+        <h1>Cadastrando usuário</h1>
+
+        <label>Nome:</label>
+        <br />
+        <input
+          placeholder="Digite seu nome" 
+          value={nome}
+          onChange={ ( event ) => setNome(event.target.value)}    
+        />
+        <br />
+
+        <label>Idade:</label>
+        <br />
+        <input
+          type="number"
+          placeholder="Digite sua idade"
+          value={idade}
+          onChange={ ( event ) => setIdade(Number(event.target.value))}  
+        />
+        <br />
+
+        <label>Email:</label>
+        <br />
+        <input
+          placeholder="Digite sua email"
+          value={email}
+          onChange={ ( event ) => setEmail(event.target.value)}    
+        />
+        <br />
+
+        <button type="submit">Registrar</button>
+
+        <br /><br />
+
         <div>
-            <h1>Componente App</h1> <br />
-            <h2>Olá: {aluno}</h2>
 
-            <button onClick={ () =>handleChangeName('Jaasiel') }>
-                Mudar Nome
-            </button>
+          <span>Bem vindo: {user.nome}</span>
+          <br />
+          
+          <span>Idade: {user.idade}</span>
+          <br />
+
+          <span>Email: {user.email}</span>
+          <br />
+
         </div>
-    );
+
+      </form>
+
+    </div>
+  );
 }
 
 export default App;
--- modulo11-reactjs/primeiro-app/src/components/Nome.tsx
+++ modulo11-reactjs/primeiro-app/src/components/Nome.tsx
@@ -1,12 +1,12 @@
 type NomeProps = {
-    aluno: string;
-    idade: number;
+  aluno: string;
+  idade: number;
 }
 
 function Nome({aluno, idade}: NomeProps){
-    return(
-        <span>Bem-vindo: {aluno} - Idade: {idade} anos</span>
-    )
+  return(
+    <span>Bem-vindo: {aluno} - Idade: {idade} anos</span>
+  )
 }
 
 export default Nome;
