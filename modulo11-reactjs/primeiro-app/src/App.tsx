ccd8f698e6818b2c74bf8b829ae297d347bbf99b
cj-bahia <carlosj.sbahia@gmail.com>
2026-05-17 22:57:49 -0400

    Aula 04 - Conhecendo o useState

--- modulo11-reactjs/primeiro-app/src/App.tsx
+++ modulo11-reactjs/primeiro-app/src/App.tsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
+
 import Nome from "./components/Nome";
 
 function App(){
+    const[aluno, setAluno] = useState('Sujeito Programador');
+
+    function handleChangeName(nome: string){
+        setAluno(nome);
+    }
+
     return(
         <div>
-            <h1>Componente App</h1>
-            <Nome aluno="Lucas" idade={30}/>
-            <br/>
-            <Nome aluno="Jose Silva" idade={25}/>
+            <h1>Componente App</h1> <br />
+            <h2>Olá: {aluno}</h2>
+
+            <button onClick={ () =>handleChangeName('Jaasiel') }>
+                Mudar Nome
+            </button>
         </div>
     );
 }
