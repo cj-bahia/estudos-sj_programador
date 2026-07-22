906e7ae7c93e375c0b4aec9e833cb1ad33430b5d
cj-bahia <carlosj.sbahia@gmail.com>
2026-07-22 15:07:09 -0400

    Aula 07 - Estilizando detalhes do filme

--- modulo12-projeto-primeflix/Prime Flix/src/pages/Filme/index.tsx
+++ modulo12-projeto-primeflix/Prime Flix/src/pages/Filme/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
+import './filme-info.css'
 import api from "../../services/api"
 
 interface Filme{
@@ -52,6 +53,15 @@ function Filme() {
       <span>{filme.overview}</span>
 
       <strong>Avaliação: {filme.vote_average} / 10</strong>
+
+      <div className="area-buttons">
+        <button>Salvar</button>
+        <button>
+          <a href="#">
+            Trailer
+          </a>
+        </button>
+      </div>
     </div>
   )
 }
