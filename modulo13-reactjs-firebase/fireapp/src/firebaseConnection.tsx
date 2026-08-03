888fb13b794bd409b98bebd914b52099f968e8e7
cj-bahia <carlosj.sbahia@gmail.com>
2026-08-03 18:04:49 -0400

    Aula 01 - Configurando firebase

---
+++ modulo13-reactjs-firebase/fireapp/src/firebaseConnection.tsx
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBtvXVEcLATSDSCU_ZwFK9LENlE3igiJeI",
+  authDomain: "curso-2d6d5.firebaseapp.com",
+  projectId: "curso-2d6d5",
+  storageBucket: "curso-2d6d5.firebasestorage.app",
+  messagingSenderId: "74736057772",
+  appId: "1:74736057772:web:e397d388dfa1e673518e09",
+  measurementId: "G-CEBYJ7N12V",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const db = getFirestore(firebaseApp);
+
+export { db };
