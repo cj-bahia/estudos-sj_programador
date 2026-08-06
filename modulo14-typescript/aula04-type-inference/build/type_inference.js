0a15f3fe7132318b099095795efff71b978e7e15
cj-bahia <carlosj.sbahia@gmail.com>
2026-08-06 19:50:31 -0400

    Aula 04 - Type inference

---
+++ modulo14-typescript/aula04-type-inference/build/type_inference.js
@@ -0,0 +1,8 @@
+"use strict";
+// Tipagem dinâmica
+let curso = "TypeScript";
+let tecnologias = ["PHP", "React JS", 2026];
+tecnologias.push("React Native");
+tecnologias.push(2026);
+//! tecnologias.push(false);    Não aceita tipo booleano no array
+console.log(tecnologias);
---
+++ modulo14-typescript/aula04-type-inference/src/type_inference.ts
@@ -0,0 +1,13 @@
+// Tipagem dinâmica
+
+let curso = "TypeScript";
+
+let tecnologias = ["PHP", "React JS",  2026];
+
+tecnologias.push("React Native");
+
+tecnologias.push(2026);
+
+//! tecnologias.push(false);    Não aceita tipo booleano no array
+
+console.log(tecnologias);
